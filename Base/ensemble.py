ab03ba951400d2a18148f211e6cfbbf239b861fb
Kim dong hyun, 김동현 <rkdqus2006@naver.com>
2021-09-04 09:18:28 +0900

    Update

--- Base/ensemble.py
+++ Base/ensemble.py
@@ -1,20 +1,33 @@
+###################
+# import packages #
+###################
+
 import pandas as pd
 import glob
 
+#############
+# Functions #
+#############
+
+# CSV를 불러와 결과를 Hard Voting하는 함수 입니다.
 def Hardvoting(data_dir,save_file_name):
+    # 결과 CSV path
     all_files = glob.glob(data_dir +'*.csv") 
     li = []
-
+  
+    # file pat에 따라 csv 로드
     for filename in all_files:
         df = pd.read_csv(filename, index_col=None, header=0)
         li.append(df['ans'])
-
+    
+    # 결과 종합 concatnate
     frame = pd.concat(li, axis=1, ignore_index=True)
 
     df['ans'] = frame.mode(axis=1)[0]
     df['ans'] = df['ans'].astype(int)
     df.to_csv(data_dir+save_file_name'./save_file_name.csv')
                           
+# 라벨을 Encoding 하는 함수 입니다.                          
 def Make_label(data_dir,save_file_name,label_list):
     all_files = glob.glob(data_dir +'*.csv") 
     li = []
