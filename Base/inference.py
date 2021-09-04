4f1c93d942f7738b9e137456b021654a83f1a5b3
Kim dong hyun, 김동현 <rkdqus2006@naver.com>
2021-09-04 09:55:43 +0900

    Update comments

--- Base/inference.py
+++ Base/inference.py
@@ -1,43 +1,46 @@
+###################
+# import packages #
+###################
+
 import argparse
 import os
 from importlib import import_module
-
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader
-
 from dataset import TestDataset, MaskBaseDataset
 
+############
+# Function #
+############
 
-def load_model(saved_model, num_classes, device):
+# 모델 로드
+def load_model(saved_model_dir, num_classes, device):
     model_cls = getattr(import_module("model"), args.model)
     model = model_cls(
         num_classes=num_classes
     )
-
-    # tarpath = os.path.join(saved_model, 'best.tar.gz')
-    # tar = tarfile.open(tarpath, 'r:gz')
-    # tar.extractall(path=saved_model)
-
-    model_path = os.path.join(saved_model, '4_12_accuracy_91.10%_f1_79.40%.pth')   # need to change
+    
+    model_path = os.path.join(saved_model_dir, '4_12_accuracy_91.10%_f1_79.40%.pth')   # 필요한 모델의 path로 바꿔가며 사용
     model.load_state_dict(torch.load(model_path, map_location=device))
 
     return model
-"""
-0_6_accuracy_92.64%_f1_85.22%.pth
-1_16_accuracy_90.97%_f1_80.67%.pth
-2_11_accuracy_90.25%_f1_81.96%.pth
-3_11_accuracy_91.74%_f1_80.41%.pth
-4_12_accuracy_91.10%_f1_79.40%.pth
-"""
 
 @torch.no_grad()
 def inference(data_dir, model_dir, output_dir, args):
     """
+        Model inference
+            - data_dir : str, data directory path
+            - model_dir : str, model directory path
+            - output_dir : str, inference result directory path
+            - args : arguements that apply to model
     """
+    
+    #Set device
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
-
+    
+    #Set the number of class
     if args.label == "age":
         num_classes = 3
     elif args.label == "gender":
@@ -46,15 +49,20 @@ def inference(data_dir, model_dir, output_dir, args):
         num_classes = 3
     else:
         num_classes = 18
-#    num_classes = MaskBaseDataset.num_classes  # 18
+    
+    #load model
     model = load_model(model_dir, num_classes, device).to(device)
     model.eval()
-
+    
+    #set path and get information
     img_root = os.path.join(data_dir, 'newimages')
     info_path = os.path.join(data_dir, 'info.csv')
     info = pd.read_csv(info_path)
-
+    
+    #get each images path 
     img_paths = [os.path.join(img_root, img_id) for img_id in info.ImageID]
+    
+    #set inference DataLoader
     dataset = TestDataset(img_paths, args.resize)
     loader = torch.utils.data.DataLoader(
         dataset,
@@ -64,7 +72,8 @@ def inference(data_dir, model_dir, output_dir, args):
         pin_memory=use_cuda,
         drop_last=False,
     )
-
+    
+    #inference
     print("Calculating inference results..")
     preds = []
     with torch.no_grad():
@@ -73,11 +82,16 @@ def inference(data_dir, model_dir, output_dir, args):
             pred = model(images)
             pred = pred.argmax(dim=-1)
             preds.extend(pred.cpu().numpy())
-
     info['ans'] = preds
+    
+    #Save result
     info.to_csv(os.path.join(output_dir, f'output.csv'), index=False)
     print(f'Inference Done!')
 
+    
+########
+# main #
+########
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
